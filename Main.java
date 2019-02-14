f84265a9fdaf02491f41cbdb62c95da88715cea4
Taras Dranik <47506464+TarasTewari-Dranik@users.noreply.github.com>
2019-02-14 22:19:35 +0100

    Strings operation

--- Main.java
+++ Main.java
@@ -1,45 +1,22 @@
-package specification.assignment.area.calculation;
+package specification.assignment.strings.operation;
 import java.util.Scanner;
 
-
-public class Main {
-    
-
+class Main {
     public static void main(String[] args) {
-        int choice;
-        Scanner input = new Scanner(System.in);
-        do {
-            System.out.print("Area of which shape do you want to calculate?"+"\n");
-            System.out.print("1 for Triangle; 2 for Rectangle; 3 for Square;4 for Circle; O for Exit"+"\n");
-            choice = input.nextInt();
-            switch (choice) {
-            case 1:
-                System.out.print("Provide width"+"\n");
-                double widthTriangle = input.nextDouble();
-                System.out.print("Provide height"+"\n");
-                double heightTriangle = input.nextDouble();
-                System.out.printf("Area of triangle is " +new Area().triangleArea(widthTriangle, heightTriangle)+"\n");
-                break;
-            case 2:
-                System.out.print("Provide width"+"\n");
-                double widthRectangle = input.nextDouble();
-                System.out.print("Provide height"+"\n");
-                double heightRectangle = input.nextDouble();
-                System.out.printf("Area of rectangle is " +new Area().rectangleArea(widthRectangle, heightRectangle)+"\n");
-                break;
-            case 3:
-                System.out.print("Provide width"+"\n");
-                double widthSquare = input.nextDouble();
-                System.out.printf("Area of square is " +new Area().squareArea(widthSquare)+"\n");
-                break;
-            case 4:
-                System.out.print("Provide radius"+"\n");
-                double radius = input.nextDouble();
-                System.out.printf("Area of square is " +new Area().circleArea(radius)+"\n");
-                break;
-            default:
-                break;
-            }
-        } while (choice != 0);
+        System.out.print("Compare two strings and return 1 if equals else 0."+"\n");
+        Scanner userFirstString = new Scanner(System.in);
+        System.out.print("Enter first string: "+"\n");
+        String firstString = userFirstString.nextLine();
+        Scanner userSecondString = new Scanner(System.in);
+        System.out.print("Enter second string: "+"\n");
+        String secondString = userSecondString.nextLine();
+        System.out.print(new StringOperation().stringComparison(firstString, secondString)+"\n");
+        System.out.print("Return the reverse string, replace lowercase characters with uppercase and vice-versa, return the largest word of a string."+"\n");
+        Scanner userString = new Scanner(System.in);
+        System.out.print("Enter string: "+"\n");
+        String operationString = userString.nextLine();
+        System.out.print("The reverse string: "+new StringOperation().reverseString(operationString)+"\n");
+        System.out.print("Lowercase characters with uppercase and vice-versa "+ new StringOperation().replaceCharacters(operationString)+"\n");
+        System.out.print("Largest word: "+new StringOperation().largestWord(operationString)+"\n");
     }
 }
